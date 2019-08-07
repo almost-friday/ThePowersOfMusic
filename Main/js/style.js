4cbf4d60657eaeaeef33a37bec0c31d32ad27093
Smaxter_X <chickenfatbubble@gmail.com>
2019-08-07 13:53:17 -0700

    Started the header scrolling animation
    Also slightly changed the nav bar

--- Main/js/style.js
+++ Main/js/style.js
@@ -1,16 +1,21 @@
-function changeHeaderBG() {
-    let elt = document.getElementById("head");
+function headerScroll() {
+    let head = document.getElementById("header");
+    let logo = document.querySelector("#header .logo");
+    let name = document.getElementById("header_name");
+    let message = {failure: "headerScroll() has errors:\n\t"}
+    if (!head || !logo || !name) fatal("Your javascript code needs to be updated.");
 
     if(this.scrollY > 1) {
-        if(elt.classList.contains("show_head_bg")) elt.classList.remove("show_head_bg");
-        elt.classList.remove("animate_head_bg");
-        elt.classList.add("animate_head_no_bg");
+        logo.classList.add("scroll");
     }
     else {
-        elt.classList.remove("animate_head_no_bg");
-        elt.classList.add("animate_head_bg");
+        logo.classList.remove("scroll");
+    }
+    function fatal(msg) {
+        console.log(message.failure + msg);
+        return -1;
     }
 }
 
 //This just changes the header background when scrolling
-window.addEventListener("scroll", changeHeaderBG, false);
+window.addEventListener("scroll", headerScroll, false);
